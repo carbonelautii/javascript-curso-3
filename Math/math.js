83c9173e33db93b6b5eb8d3682d55757a20e9685
Lautaro Carbone <carbonelauti@gmail.com>
2022-10-14 02:26:19 -0300

    Triangulos Escalenos

--- Math/math.js
+++ Math/math.js
@@ -38,6 +38,15 @@ function calcularTriangulo(lado1, lado2, base, altura) {
   };
 }
 
+function calcularAlturaTriangulo(lado1, base) {
+  if (lado1 == base) {
+    console.warn("Este no es un triángulo isosceles");
+  } else {
+    // H = raizCuadrada (lado1**2 - (b**2)/4)
+    return Math.sqrt(lado1 ** 2 - base ** 2 / 4);
+  }
+}
+
 console.log({
   ladoTriangulo1,
   ladoTriangulo2,
@@ -75,3 +84,23 @@ console.log({
 });
 
 console.groupEnd("Circle");
+
+function calcularAlturaTrianguloEscaleno(lado1, lado2, base) {
+  if (lado1 == lado2 || lado2 == base || base == lado1) {
+    console.warn("Esto no es un triángulo Escaleno");
+  } else {
+    // semiPerimetro = (lado1 + lado2 + base) / 2
+    const semiPerimetro = (lado1 + lado2 + base) / 2;
+    // h = (2 / lado1) * raizCuadrada (semiPerimetro(semiperimetro-lado1) * (semiPerimetro(semiperimetro-lado2) * (semiPerimetro(semiperimetro-lado3) )
+    const altura =
+      (2 / lado1) *
+      Math.sqrt(
+        (semiPerimetro *
+          (semiPerimetro - lado1) *
+          (semiPerimetro - lado2) *
+          (semiPerimetro - base)) /
+          2
+      );
+    return altura;
+  }
+}
