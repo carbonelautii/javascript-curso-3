88016c2710b1d604eae98a0f39084b819716a838
Lautaro Carbone <carbonelauti@gmail.com>
2022-10-17 19:25:46 -0300

    descuentos a productos

---
+++ Porcentaje/porcentaje.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputDiscount = document.querySelector("#discount").value;
+  const inputPrice = document.querySelector("#price").value;
+
+  if (inputPrice === "" || inputDiscount === "") {
+    showResults("Revisa que los campos esten llenos");
+  } else {
+    const price = parseInt(inputPrice);
+    const discount = parseInt(inputDiscount);
+
+    const total = (price * (100 - discount)) / 100;
+
+    showResults(`${total}$`);
+  }
+});
+
+function showResults(total) {
+  const div = document.querySelector(".results");
+
+  div.removeChild(div.firstChild);
+
+  const p = document.createElement("p");
+  p.innerText = total;
+
+  div.appendChild(p);
+}
