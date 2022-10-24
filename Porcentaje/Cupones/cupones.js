1f212ff30a61baa42d1e072d8f1711192f3c1a93
Lautaro Carbone <carbonelauti@gmail.com>
2022-10-24 01:27:54 -0300

    Cupones con objetos y metodos en arrays

--- Porcentaje/Cupones/cupones.js
+++ Porcentaje/Cupones/cupones.js
@@ -5,6 +5,20 @@ const div = document.querySelector("#results");
 
 btn.addEventListener("click", calcularDescuento);
 
+const couponList = [];
+couponList.push({
+  name: "CUPON1",
+  discount: 30,
+});
+couponList.push({
+  name: "CUPON2",
+  discount: 20,
+});
+couponList.push({
+  name: "CUPON3",
+  discount: 10,
+});
+
 function calcularDescuento(event) {
   // (P * (100 - D)) / 100
   event.preventDefault();
@@ -16,37 +30,71 @@ function calcularDescuento(event) {
     alert("Por favor, completa los campos vacios!");
     return;
   }
+
   let discount;
 
-  // LOGICA CON SWITCH (BORRAR COMENTADO PARA TESTEAR)
-
-  /*
-  switch (coupon) {
-    case "CUPON1":
-      discount = 30;
-      break;
-
-    case "CUPON2":
-      discount = 25;
-      break;
-
-    default:
-      break;
+  function isCouponInArray(couponElement) {
+    // {name, discount}
+    return couponElement.name == coupon;
   }
-*/
 
-  // LOGICA CON IF & ELSE (BORRAR COMENTADO PARA TESTEAR)
-  /*
-  if (coupon == "CUPON1") {
-    discount = 30;
-  } else if (coupon == "CUPON2") {
-    discount = 25;
+  const couponInArray = couponList.find(isCouponInArray); // {}
+
+  if (couponInArray) {
+    discount = couponInArray.discount;
   } else {
-    div.innerText = "El nuevo precio con descuento es $" + resultadoFinal;
+    div.innerText = "El cupón no es válido!";
     return;
   }
-*/
-  const resultadoFinal = (price * (100 - discount)) / 100;
 
+  console.log({
+    coupon,
+    discount,
+    couponInArray,
+    couponList,
+  });
+
+  // --------------------------------------------------------------------------------
+  // LOGICA CON OBJECT (BORRAR COMENTADO PARA TESTEAR)
+  // const couponObj = {
+  //   CUPON1: 30,
+  //   CUPON2: 15,
+  //   CUPON3: 10,
+  // };
+
+  // if (couponObj[coupon]) {
+  //   discount = couponObj[coupon];
+  // } else {
+  // div.innerText = "El nuevo precio con descuento es $" + resultadoFinal;
+  // return;
+  // }
+  // --------------------------------------------------------------------------------
+  // LOGICA CON SWITCH (BORRAR COMENTADO PARA TESTEAR)
+
+  // switch (coupon) {
+  //   case "CUPON1":
+  //     discount = 30;
+  //     break;
+
+  //   case "CUPON2":
+  //     discount = 25;
+  //     break;
+
+  //   default:
+  //     break;
+  // }
+  // --------------------------------------------------------------------------------
+  // LOGICA CON IF & ELSE (BORRAR COMENTADO PARA TESTEAR)
+
+  // if (coupon == "CUPON1") {
+  //   discount = 30;
+  // } else if (coupon == "CUPON2") {
+  //   discount = 25;
+  // } else {
+  // div.innerText = "El nuevo precio con descuento es $" + resultadoFinal;
+  // return;
+  // }
+  // --------------------------------------------------------------------------------
+  const resultadoFinal = (price * (100 - discount)) / 100;
   div.innerText = "El nuevo precio con descuento es $" + resultadoFinal;
 }
