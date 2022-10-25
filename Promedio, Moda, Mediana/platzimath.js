2e2bede0c80b7048aa4cb2abfc5db96dda2c201d
Lautaro Carbone <carbonelauti@gmail.com>
2022-10-25 20:12:50 -0300

    Metodo sort

--- Promedio, Moda, Mediana/platzimath.js
+++ Promedio, Moda, Mediana/platzimath.js
@@ -5,6 +5,26 @@ function esImpar(array) {
   return array.length % 2;
 }
 
+function ordenarLista(listaDesordenada) {
+  function ordenarListaSort(valorAcumulado, nuevoValor) {
+    if (valorAcumulado > nuevoValor) {
+      return 1;
+    } else if (valorAcumulado == 0) {
+      return 0;
+    } else if (valorAcumulado < nuevoValor) {
+      return -1;
+    }
+
+    return valorAcumulado - nuevoValor; // Resumir condicional,
+    // 10 - 5: 5 (Numero positivo (Retorna "1"))
+    // 5 - 5: 0 (Retorna "0")
+    // 5 - 10: -5 (Numero negativo (Retorna "-1"))
+  }
+
+  const lista = listaDesordenada.sort(ordenarListaSort);
+  return lista;
+}
+
 function calcularMediana(array) {
   const listaEsPar = esPar(array);
 
